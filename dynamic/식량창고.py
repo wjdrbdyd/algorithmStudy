d47765db63d0b6664a1b17b5e35a45b82c0a2d9f
wjdrbdyd <jky9010@gmail.com>
2021-12-22 13:54:30 +0900

    Add files via upload

---
+++ dynamic/식량창고.py
@@ -0,0 +1,14 @@
+# 식량 창고 개수
+n = int(input())
+# 식량창고에 저장된 식량의 개수
+k = list(map(int, input().split()))
+
+d = [0] * 100
+
+d[0] = k[0]
+d[1] = max(k[0], k[1])
+
+for i in range(2, n):
+  d[i] = max(d[i - 1], d[i-2] + k[i])
+
+print(d[n-1])
