c1f5e1b8fa2efcc693a368240314dffc5f56e042
wjdrbdyd <jky9010@gmail.com>
2022-01-03 19:09:02 +0900

    Update 미로탈출.py

--- dfsbfs/미로탈출.py
+++ dfsbfs/미로탈출.py
@@ -1,36 +1,35 @@
 from collections import deque
 n, m = map(int, input().split())
 graph = []
-distance = 0
-for _ in range(n):
-  graph.append(list(map(int, input())))
 
-# 이동할 네 방향 정의(상, 하, 좌, 우)
+for i in range(1, n + 1):
+  graph.append(list(map(int, input())))
+# 상 하 좌 우 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y):
-  queue = deque()
-  queue.append((x, y))
+def bfs(a, b):
+  # 큐에 처음 시작 추가
+  queue = deque([(a, b)])
+  # 큐가 빌 때까지 반복
   while queue:
+    # 현재 위치 꺼내서 
     x, y = queue.popleft()
-    print(f'x: {x}, y: {y}')
-    # 현재 위치에서 네 방향으로 위치 확인
+    # 현재 위치에서 상하 좌우 탐색
     for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      # 공간 벗어난 경우
+      nx = dx[i] + x
+      ny = dy[i] + y
       if nx < 0 or nx >= n or ny < 0 or ny >= m :
         continue
-      # 벽인 경우 무시 (0인 경우 )
-      if graph[nx][ny] == 0 :
+      if graph[nx][ny] == 0 : 
         continue
-      # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-      if graph[nx][ny] == 1 :
+      # 해당 노드를 처음 방문하는 경우만 최단 거리 기록
+      # graph[nx][ny] != 0 인걸로 조건 걸었기 때문에 무한루프 빠짐.
+      # 상하 좌우 0이아닌거 계속 상하좌우 탐색하며 큐에 추가하게 된다.
+      # 조건 생각을 잘못했다.
+      if graph[nx][ny] == 1:
         graph[nx][ny] = graph[x][y] + 1
         queue.append((nx, ny))
-  # 가장 오른쪽 아래까지의 최단 거리 반환
   return graph[n - 1][m - 1]
-print(bfs(0,0))
-print(graph)
-
+    
+print(bfs(1, 1))
